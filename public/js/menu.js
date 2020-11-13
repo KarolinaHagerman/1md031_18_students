1a3c69a0308c8a620cf6f81da7d326840bcaa311
fasterfiffi <hagermankarolina@gmail.com>
2020-11-13 18:00:21 +0100

    Skapat alla burgare

---
+++ public/js/menu.js
@@ -0,0 +1,56 @@
+//JSON
+export let menu = [
+{
+  "name":"Basic Burger",
+  "imgURL":"https://www.max.se/contentassets/7283b5a0bf324ba99d7fa7e0caf5ae7f/product_max-original.png",
+  "kcal":"750",
+  "gluten":"true",
+  "lactos":"true",
+  "description":"Perfect if you want to be really basic"
+},
+
+{
+  "name":"Boring Burger",
+  "imgURL":"https://www.max.se/contentassets/c02201e316004a52a08daff936eaa7c9/product_veggieburger.png",
+  "kcal":"600",
+  "gluten":"true",
+  "lactos":"false",
+  "description":"Perfect if you want to be really boring"
+},
+
+{
+  "name":"Chicken Burger",
+  "imgURL":"https://www.max.se/contentassets/23e325ee1e224f1e9ea8314c346e34c3/product_grilled-chicken-burger.png",
+  "kcal":"850",
+  "gluten":"true",
+  "lactos":"true",
+  "description":"Perfect if you want to be a chicken"
+},
+
+{
+  "name":"Nacho Burger",
+  "imgURL":"https://www.max.se/contentassets/f6eff8ddf03441ac9ba3948ecff1fb04/product_crunchy-nacho-burger.png",
+  "kcal":"900",
+  "gluten":"true",
+  "lactos":"true",
+  "description":"Perfect if you want to be spicy"
+},
+
+{
+  "name":"Meaty Burger",
+  "imgURL":"https://www.max.se/contentassets/942a92f9f21b48cb859c1e8dad5f8f30/product_delivery-cheezy-burger.png?width=1024&sharpen=5&sigma=1,4&threshold=0",
+  "kcal":"900",
+  "gluten":"true",
+  "lactos":"true",
+  "description":"Perfect if you want lots of meat"
+},
+
+{
+  "name":"Bacon Burger",
+  "imgURL":"https://www.max.se/contentassets/c5cfa72f1a794f92a587e5224edfad9b/product_grand-deluxe-cheese-n-bacon.png",
+  "kcal":"850",
+  "gluten":"true",
+  "lactos":"true",
+  "description":"Perfect if you want your bacon"
+}
+]
