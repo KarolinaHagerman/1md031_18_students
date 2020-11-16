f3bef6833e11ffe90747988a38a3f0fc49fb1b9d
fasterfiffi <hagermankarolina@gmail.com>
2020-11-16 16:20:55 +0100

    klar med 4.14.3

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,10 +1,22 @@
-import { MenuItem } from './js_script.js'
+import { placeOrder, getInputVals } from './js_script.js'
 import { menu } from './menu.js'
 
 new Vue({
   el: "#menu-app",
   data: {
   	items: menu
-
   },
 })
+
+new Vue({
+    el: '#orders',
+    methods: {
+        markDone: function () {
+            console.log('Button clicked!');
+        }
+    }
+});
+
+//DON'T NEED FUNCTIONS FOR THESE, ALL IS WRITTEN IN MY index.html
+//A function that loops through the array and inserts the information to the burger selection section of the index.html file
+//A function that loads the information from the menu.js json object and inserts the information to the burger selection section of the index.html file
