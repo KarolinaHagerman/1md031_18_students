d6e276a45dc84172208494daa1c0ecb784ba27eb
fasterfiffi <hagermankarolina@gmail.com>
2020-11-19 15:16:38 +0100

    FÄRDIG, denna innehåller vue-lösningen och fungerar även med dispatcher

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -11,59 +11,6 @@ new Vue({
   },
 })
 
-//INNAN JAG BÖRJADE MED UPPGIFT 2 (ANDRA) PÅ MESSAGING DVS TVÅ OBJEKT
-// new Vue({
-//     el: '#orders',
-//     methods: {
-//       markDone: function() {
-//           placeOrder();
-//       }
-//     }
-// });
-//
-//  new Vue({
-//   el: '#dots',
-//   data: {
-//     orders: {},
-//     newOrder: {}
-//   },
-//   created: function () {
-//     socket.on('initialize', function (data) {
-//       this.orders = data.orders;
-//     }.bind(this));
-//
-//     socket.on('currentQueue', function (data) {
-//       this.orders = data.orders;
-//     }.bind(this));
-//   },
-//   methods: {
-//     getNext: function () {
-//       var lastOrder = Object.keys(this.orders).reduce(function (last, next) {
-//         return Math.max(last, next);
-//       }, 0);
-//       return lastOrder + 1;
-//     },
-//     addOrder: function (event) {
-//       console.log("INNE I ADDORDER");
-//       var offset = {x: event.currentTarget.getBoundingClientRect().left,
-//                     y: event.currentTarget.getBoundingClientRect().top};
-//       socket.emit("addOrder", { orderId: this.getNext(),
-//                                 details: { x: event.clientX - 10 - offset.x,
-//                                            y: event.clientY - 10 - offset.y },
-//                                 orderItems: ["Beans", "Curry"]
-//                               });
-//     },
-//     displayOrder: function (event){
-//       var offsets = {x: event.currentTarget.getBoundingClientRect().left,
-//                     y: event.currentTarget.getBoundingClientRect().top};
-//
-//       this.newOrder = ( { details: { x: event.clientX - 10 - offsets.x,
-//                  y: event.clientY - 10 - offsets.y },
-//                });
-//     }
-//   }
-// });
-
 new Vue({
  el: '#orderSection',
  data: {
@@ -117,17 +64,6 @@ new Vue({
      }, 0);
      return lastOrder + 1;
    },
-   //GAMLA addOrder
-   // addOrder: function (event) {
-   //   console.log("INNE I ADDORDER");
-   //   var offset = {x: event.currentTarget.getBoundingClientRect().left,
-   //                 y: event.currentTarget.getBoundingClientRect().top};
-   //   socket.emit("addOrder", { orderId: this.getNext(),
-   //                             details: { x: event.clientX - 10 - offset.x,
-   //                                        y: event.clientY - 10 - offset.y },
-   //                             orderItems: ["Beans", "Curry"]
-   //                           });
-   // },
    addOrder: function (event) {
      socket.emit("addOrder", { orderId: this.getNext(),
                                details: this.newOrder,
@@ -139,9 +75,8 @@ new Vue({
      var offsets = {x: event.currentTarget.getBoundingClientRect().left,
                    y: event.currentTarget.getBoundingClientRect().top};
 
-     this.newOrder = ( { details: { x: event.clientX - 10 - offsets.x,
-                y: event.clientY - 10 - offsets.y },
-              });
+     this.newOrder = ( { x: event.clientX - 10 - offsets.x,
+                y: event.clientY - 10 - offsets.y });
    }
  }
 });
